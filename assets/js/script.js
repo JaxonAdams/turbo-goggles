36edcb0e0043763c87f58f773ac63c67bf9823ec
Jaxon Adams <jadamsresume1@gmail.com>
2022-08-08 14:08:08 -0600

    keys control the paddle

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,20 @@ let ballPosX = canvas.width / 2;
 let ballPosY = canvas.height / 2;
 
 // ball velocity
-let ballDX = 1.5;
-let ballDY = -1.5;
+let ballDX = 1.7;
+let ballDY = -1.7;
 
 // ball radius
 const ballRadius = 10;
+
+// paddle
+const paddleWidth = 75;
+const paddleHeight = 10;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// arrow keys pressed
+let rightPressed = false;
+let leftPressed = false;
 
 // draw the ball
 const drawBall = () => {
@@ -22,13 +31,23 @@ const drawBall = () => {
     ctx.closePath();
 };
 
+// draw the paddle
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = 'tomato';
+    ctx.fill();
+    ctx.closePath();
+};
+
 // drawing loop function
 const draw = () => {
     // clear previous frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // paint ball to screen
+    // paint assets to the screen
     drawBall();
+    drawPaddle();
 
     // move ball
     ballPosX += ballDX;
@@ -43,7 +62,35 @@ const draw = () => {
     if (ballPosX + ballDX < ballRadius || ballPosX + ballDX > canvas.width - ballRadius) {
         ballDX = -ballDX;
     };
+
+    // key pressed
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + 5, canvas.width - paddleWidth);
+    } else if (leftPressed) {
+        paddleX = Math.max(paddleX - 5, 0);
+    };
 };
+
+// key pressed handler functions
+const keyDownHandler = e => {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = true;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = true;
+    };
+};
+
+const keyUpHandler = e => {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+        rightPressed = false;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        leftPressed = false;
+    };
+};
+
+// key event listeners
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
 
 // run drawing loop every 10 milliseconds
 setInterval(draw, 10);
